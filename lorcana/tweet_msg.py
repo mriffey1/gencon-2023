9dea441de28d4982ca68a80d23406a698f90500b
MeganRiffey <mriffe4@wgu.edu>
2023-06-19 15:39:07 -0400

    updated formatting

--- lorcana/tweet_msg.py
+++ lorcana/tweet_msg.py
@@ -6,7 +6,6 @@ dbhost, dbusername, dbpassword, dbname = utils_lorcana.database_stuff()
 
 tbearer, tckey, tcsecret, tatoken, tasecret = utils_lorcana.twitter_stuff()
 
-
 def send_email(event_datetime, event_title, ticket_amount, event_url, email_type):
     if "Lorcana" in event_title:
         api = tweepy.Client(
@@ -18,9 +17,8 @@ def send_email(event_datetime, event_title, ticket_amount, event_url, email_type
         )
         now = datetime.datetime.today()
         dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
+        
         # authentication of consumer key and secret
         tweet = api.create_tweet(
             text=f"{event_title}\n{event_datetime}\n{ticket_amount} ticket(s)\n{event_url}\nChecked {dt_string}",
         )
-    else:
-        print("No tickets")
